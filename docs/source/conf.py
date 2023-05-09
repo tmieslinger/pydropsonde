8bba9e7f80d5bc03a4ce4b0eb0ee37e0d90975ba
Geet George <46085279+Geet-George@users.noreply.github.com>
2023-05-09 18:09:09 +0200

    include links to source code in readthedocs (#6)
    add src to path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('../../src'))
+
 project = 'HALO-DROPS'
 copyright = '2023, Geet George'
 author = 'Geet George'
