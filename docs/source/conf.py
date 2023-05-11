7dad03af855fe5d1ed8bd6571e1c4b5d97bb79a2
Geet George <geet.george@mpimet.mpg.de>
2023-05-11 07:47:53 +0200

    add github source buttons

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,14 @@ source_suffix = {
 
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
-
+html_context = {
+   "default_mode": "light"
+}
+html_theme_options = {
+    "repository_url": "https://github.com/Geet-George/halodrops",
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+}
 # Set link name generated in the top bar.
 html_title = 'HALO-DROPS'
