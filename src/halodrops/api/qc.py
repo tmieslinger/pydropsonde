c18c2bbd90ccf874fdc0a4aad8786e8bbe6b8320
Geet George <g.george@tudelft.nl>
2023-10-31 14:42:30 +0100

    misc changes to test inspect working

--- src/halodrops/api/qc.py
+++ src/halodrops/api/qc.py
@@ -3,14 +3,15 @@ from halodrops.helper import paths
 from halodrops.qc import profile
 
 
-def run(data_directory="default", flight_id="20220401"):
+def run(data_directory, flight_id):
     paths_for_flight = paths.Paths(data_directory, flight_id)
     # Create Sondes dictionary
     Sondes = paths_for_flight.populate_sonde_instances()
-    ds = Sondes[list(Sondes.keys())[10]].aspen_ds
+    [print(f"{i}: {Sondes[i].launch_detect}") for i in list(Sondes.keys())]
+    ds = Sondes[list(Sondes.keys())[1]].aspen_ds
     var = "tdry"
     return print(
-        f"---------------------\n For Sonde {list(Sondes.keys())[10]} the {var} variable has a weighted profile fullness of {profile.weighted_fullness(ds,var,sampling_frequency=2):.02f}. Yay!"
+        f"---------------------\n For Sonde {list(Sondes.keys())[1]} the {var} variable has a weighted profile fullness of {profile.weighted_fullness(ds,var,sampling_frequency=2):.02f}. Yay!"
     )
 
 
