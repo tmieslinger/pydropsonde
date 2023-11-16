3749d78ab0a9d3ce8d5a6c312c7f659999be53b7
Geet George <g.george@tudelft.nl>
2023-11-16 15:20:35 +0100

    add fns to convert to si +  another fn to get these fns

--- src/halodrops/helper/__init__.py
+++ src/halodrops/helper/__init__.py
@@ -15,3 +15,27 @@ def get_bool(s):
             raise ValueError(f"Cannot convert {s} to boolean")
     else:
         raise ValueError(f"Cannot convert {s} to boolean")
+
+
+def convert_rh_to_si(value):
+    """convert RH from % to fraction"""
+    return value / 100
+
+
+def convert_pres_to_si(value):
+    """convert pressure from hPa to Pa"""
+    return value * 100
+
+
+def convert_tdry_to_si(value):
+    """convert temperature from C to K"""
+    return value + 273.15
+
+
+def get_si_converter_function_based_on_var(var_name):
+    """get the function to convert a variable to SI units based on its name"""
+    func_name = f"convert_{var_name}_to_si"
+    func = globals().get(func_name, None)
+    if func is None:
+        raise ValueError(f"No function named {func_name} found in the module")
+    return func
