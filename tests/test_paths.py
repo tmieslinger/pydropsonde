89899300fa88c1cdc7ae3decd797233511e604ba
Geet George <g.george@tudelft.nl>
2023-12-11 22:06:57 +0100

    change tests to account for data_dir structure change from 267b8a2e07b220181db8d30a9768a74e66c3ad6e

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -2,11 +2,12 @@ from halodrops.helper import paths
 import os
 
 main_data_directory = "../sample"
+platform = "HALO"
 flightdate = "20200101"
-l1_path = os.path.join(main_data_directory, flightdate, "Level_1")
-quicklooks_path = os.path.join(main_data_directory, flightdate, "Quicklooks")
+l1_path = os.path.join(main_data_directory, platform, flightdate, "Level_1")
+quicklooks_path = os.path.join(main_data_directory, platform, flightdate, "Quicklooks")
 
-object = paths.Paths(main_data_directory, flightdate)
+object = paths.Flight(main_data_directory, flightdate, platform)
 
 
 def test_l1_path():
