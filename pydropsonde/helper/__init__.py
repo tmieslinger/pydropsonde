fix theta and q calculation

pydropsonde/helper/__init__.py
@@ -94,9 +94,6 @@ variable_compression_properties = dict(
 )
 
 
-path_to_flight_ids = "{platform}/Level_0"
-path_to_l0_files = "{platform}/Level_0/{flight_id}"
-
 l2_flight_attributes_map = {
     "True Air Speed (m/s)": "true_air_speed_(ms-1)",
     "Ground Speed (m/s)": "ground_speed_(ms-1)",
@@ -109,7 +106,8 @@ l2_flight_attributes_map = {
     "MSL Altitude (m)": "aircraft_msl_altitude_(m)",
     "Geopotential Altitude (m)": "aircraft_geopotential_altitude_(m)",
 }
-
+path_to_flight_ids = "{platform}/Level_0"
+path_to_l0_files = "{platform}/Level_0/{flight_id}"
 
 l2_filename_template = "{platform}_{launch_time}_{flight_id}_{serial_id}_Level_2.nc"
 
@@ -157,56 +155,6 @@ def get_si_converter_function_based_on_var(var_name):
     return func
 
 
-def calc_saturation_pressure(temperature_K, method="hardy1998"):
-    """
-    Calculate saturation water vapor pressure
-
-    Input
-    -----
-    temperature_K : array
-        array of temperature in Kevlin or dew point temperature for actual vapor pressure
-    method : str
-        Formula used for calculating the saturation pressure
-            'hardy1998' : ITS-90 Formulations for Vapor Pressure, Frostpoint Temperature,
-                Dewpoint Temperature, and Enhancement Factors in the Range –100 to +100 C,
-                Bob Hardy, Proceedings of the Third International Symposium on Humidity and Moisture,
-                1998 (same as used in Aspen software after May 2018)
-
-    Return
-    ------
-    e_sw : array
-        saturation pressure (Pa)
-
-    Examples
-    --------
-    >>> calc_saturation_pressure([273.15])
-    array([ 611.2129107])
-
-    >>> calc_saturation_pressure([273.15, 293.15, 253.15])
-    array([  611.2129107 ,  2339.26239586,   125.58350529])
-    """
-
-    if method == "hardy1998":
-        g = np.empty(8)
-        g[0] = -2.8365744 * 10**3
-        g[1] = -6.028076559 * 10**3
-        g[2] = 1.954263612 * 10**1
-        g[3] = -2.737830188 * 10 ** (-2)
-        g[4] = 1.6261698 * 10 ** (-5)
-        g[5] = 7.0229056 * 10 ** (-10)
-        g[6] = -1.8680009 * 10 ** (-13)
-        g[7] = 2.7150305
-
-        e_sw = np.zeros_like(temperature_K)
-
-        for t, temp in enumerate(temperature_K):
-            ln_e_sw = np.sum([g[i] * temp ** (i - 2) for i in range(0, 7)]) + g[
-                7
-            ] * np.log(temp)
-            e_sw[t] = np.exp(ln_e_sw)
-        return e_sw
-
-
 def calc_q_from_rh(ds):
     """
     Input :
@@ -219,16 +167,24 @@ def calc_q_from_rh(ds):
 
     Function to estimate specific humidity from the relative humidity, temperature and pressure in the given dataset.
     """
-    e_s = calc_saturation_pressure(ds.ta.values)
-    w_s = mpcalc.mixing_ratio(e_s * units.Pa, ds.p.values * units.Pa).magnitude
-    w = ds.rh.values * w_s
-    q = w / (1 + w)
-    ds["q"] = (ds.rh.dims, q)
+    vmr = mpcalc.mixing_ratio_from_relative_humidity(
+        ds["p"].values * units.Pa,
+        ds.ta.values * units.kelvin,
+        (ds.rh * 100) * units.percent,
+    )
+    q = mpcalc.specific_humidity_from_mixing_ratio(vmr)
+
+    ds["q"] = (ds.rh.dims, q.magnitude)
+    ds["q"].attrs = dict(
+        standard_name="specific humidity",
+        long_name="specific humidity",
+        units=str(q.units),
+    )
 
     return ds
 
 
-def calc_theta_from_T(dataset):
+def calc_theta_from_T(ds):
     """
     Input :
 
@@ -241,8 +197,13 @@ def calc_theta_from_T(dataset):
     Function to estimate potential temperature from the temperature and pressure in the given dataset.
     """
     theta = mpcalc.potential_temperature(
-        dataset.p.values * units.Pa, dataset.ta.values * units.kelvin
-    ).magnitude
-    ds["theta"] = (ds.ta.dims, theta)
+        ds.p.values * units.Pa, ds.ta.values * units.kelvin
+    )
+    ds["theta"] = (ds.ta.dims, theta.magnitude)
+    ds["theta"].attrs = dict(
+        standard_name="potential temperature",
+        long_name="potential temperature",
+        units=str(theta.units),
+    )
 
     return ds