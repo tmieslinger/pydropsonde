b04e94b6c44deafb7e40faec7c8346c8e8957cad
hgloeckner <helene.gloeckner@posteo.de>
2024-08-30 19:52:15 +0200

    fix gridded tests

--- tests/test_gridded.py
+++ tests/test_gridded.py
@@ -6,8 +6,7 @@ from pydropsonde.processor import Gridded
 sondes = None
 flight_id = "20240811"
 platform_id = "HALO"
-l3_default = f"{platform_id}_{flight_id}_Level_3.nc"
-l3_template = "{flight_id}_{platform}_Level_3.nc"
+l3_default = f"Level_3.nc"
 
 
 @pytest.fixture
@@ -21,17 +20,10 @@ def test_l3_dir(gridded):
 
 
 def test_l3_dir_name(gridded):
-    gridded.get_l3_dir(l3_dirname="test")
+    gridded.get_l3_dir(l3_dir="test")
     assert gridded.l3_dir == "test"
 
 
 def test_l3_default(gridded):
     gridded.get_l3_filename()
     assert gridded.l3_filename == l3_default
-
-
-def test_l3_template(gridded):
-    gridded.get_l3_filename(l3_filename_template=l3_template)
-    assert gridded.l3_filename == l3_template.format(
-        flight_id=flight_id, platform=platform_id
-    )
