1364d95185b1dc72ae3946107df5642542b5c8a9
hgloeckner <helene.gloeckner@posteo.de>
2025-01-31 11:02:45 +0100

    adapt tests

--- tests/test_qc.py
+++ tests/test_qc.py
@@ -22,7 +22,7 @@ data_dict = {
         "q": {"dims": ("time"), "data": np.array([np.nan, np.nan, np.nan, 0.7])},
         "p": {"dims": ("time"), "data": np.array([1.0, 10.0, 100.0, 1000.0])},
         "alt": {"dims": ("time"), "data": np.array([30.0, 20.0, 15.0, 10.0])},
-        "rh": {"dims": ("time"), "data": np.array([30.0, 20.0, np.nan, 10.0])},
+        "rh": {"dims": ("time"), "data": np.array([30.0, np.nan, np.nan, 10.0])},
     },
 }
 
@@ -42,20 +42,22 @@ def qc():
 @pytest.fixture
 def qc_vars(qc):
     qc.set_qc_variables({"q": "m s-1", "p": "Pa", "rh": "1"})
+    qc.set_qc_ds(ds)
+    qc.alt_dim = "alt"
+
     assert qc.qc_vars == {"q": "m s-1", "p": "Pa", "rh": "1"}
     return qc
 
 
 def test_profile_sparsity(qc_vars):
-    qc_vars.profile_sparsity(ds, variable_dict={"q": 2, "p": 2})
+    qc_vars.profile_sparsity(variable_dict={"q": 2, "p": 2, "rh": 4})
     assert qc_vars.qc_flags["p_profile_sparsity"]
-    assert not qc_vars.qc_flags["q_profile_sparsity"]
+    assert qc_vars.qc_flags["q_profile_sparsity"]
+    assert not qc_vars.qc_flags["rh_profile_sparsity"]
 
 
 def test_near_surface(qc_vars):
-    qc_vars.near_surface_coverage(
-        ds, alt_bounds=[0, 18], alt_dim="alt", count_threshold=2
-    )
+    qc_vars.near_surface_coverage(alt_bounds=[0, 18], count_threshold=2)
 
     assert qc_vars.qc_flags["p_near_surface"]
     assert not qc_vars.qc_flags["q_near_surface"]
@@ -67,20 +69,18 @@ def test_near_surface(qc_vars):
     "qc_flag,output",
     [
         ("p_profile_sparsity", True),
-        ("q_profile_sparsity", False),
+        ("rh_profile_sparsity", False),
         ("p_profile_sparsity,p_near_surface", True),
         ("p_profile_sparsity,q_near_surface", False),
         (None, True),
         ("all", False),
-        ("all_except_p_profile_sparsity", False),
-        ("p_profile_sparsity,rh_profile_sparsity", ValueError),
+        ("all_except_rh_profile_sparsity", False),
+        ("p_profile_sparsity,ta_profile_sparsity", ValueError),
     ],
 )
 def test_check_qc(qc_vars, qc_flag, output):
-    qc_vars.profile_sparsity(ds, variable_dict={"q": 2, "p": 2})
-    qc_vars.near_surface_coverage(
-        ds, alt_bounds=[0, 18], alt_dim="alt", count_threshold=2
-    )
+    qc_vars.profile_sparsity(variable_dict={"q": 2, "p": 2, "rh": 4})
+    qc_vars.near_surface_coverage(alt_bounds=[0, 18], count_threshold=2)
     if (type(output) is type) and issubclass(output, Exception):
         with pytest.raises(output):
             res = qc_vars.check_qc(used_flags=qc_flag)
@@ -93,15 +93,13 @@ def test_check_qc(qc_vars, qc_flag, output):
     "varname,output",
     [
         ("p", "GOOD"),
-        ("q", "BAD"),
-        ("rh", "UGLY"),
+        ("rh", "BAD"),
+        ("q", "UGLY"),
     ],
 )
 def test_add_variable_flag_to_ds(qc_vars, varname, output):
-    qc_vars.profile_sparsity(ds, variable_dict={"q": 2, "p": 2, "rh": 2})
-    qc_vars.near_surface_coverage(
-        ds, alt_bounds=[0, 18], alt_dim="alt", count_threshold=2
-    )
+    qc_vars.profile_sparsity(variable_dict={"q": 2, "p": 2, "rh": 4})
+    qc_vars.near_surface_coverage(alt_bounds=[0, 18], count_threshold=2)
     ds_out = qc_vars.add_variable_flags_to_ds(ds, varname)
     assert ds_out[f"{varname}_qc"].qc_status == output
     assert (
@@ -115,8 +113,8 @@ def test_add_variable_flag_to_ds(qc_vars, varname, output):
     [
         ({"p": "Pa"}, 0),  # GOOD
         ({"q": "1", "p": "Pa"}, 2),  # UGLY
-        ({"q": "1"}, 1),  # BAD
-        ({"rh": "1"}, 2),  # UGLY
+        ({"rh": "1"}, 1),  # BAD
+        ({"q": "1"}, 2),  # UGLY
         ({"rh": "1", "q": "1"}, 2),  # UGLY
         ({"rh": "1", "p": "1"}, 2),  # UGLY
     ],
@@ -124,10 +122,8 @@ def test_add_variable_flag_to_ds(qc_vars, varname, output):
 def test_sonde_qc(qc_vars, variables, output):
     varname = "sonde_qc"
     qc_vars.qc_vars = variables
-    qc_vars.profile_sparsity(ds, variable_dict={var: 2 for var in variables})
-    qc_vars.near_surface_coverage(
-        ds, alt_bounds=[0, 18], alt_dim="alt", count_threshold=2
-    )
+    qc_vars.profile_sparsity(variable_dict={"q": 2, "p": 2, "rh": 4})
+    qc_vars.near_surface_coverage(alt_bounds=[0, 18], count_threshold=2)
 
     ds_out = qc_vars.add_sonde_flag_to_ds(ds, varname)
     assert ds_out[varname] == output
